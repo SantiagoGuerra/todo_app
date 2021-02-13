eb668d5e02405ea166bd671646a331b2b46074a6
Santiago Guerra <guerrasantiago19@gmail.com>
2021-02-13 15:33:36 -0500

    Set up devise and create authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
+
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -37,6 +38,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
