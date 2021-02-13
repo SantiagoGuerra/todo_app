f8c5f3515443904b6e4610fbc6a06ea950b65d94
Santiago Guerra <guerrasantiago19@gmail.com>
2021-02-13 15:41:17 -0500

    Create nested scaffold with TodoItem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  resources :todo_lists
+  resources :todo_lists do
+    resources :todo_items
+  end
+  namespace :todo_list do
+    resources :todo_items
+  end
   devise_for :users
   root to: "pages#home"
 end
