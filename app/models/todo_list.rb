204651867ff9772183decd786ba4728f298c96ad
Santiago Guerra <guerrasantiago19@gmail.com>
2021-02-13 17:00:06 -0500

    Add progress bar

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,4 +1,18 @@
 class TodoList < ApplicationRecord
   has_many :todo_items
   belongs_to :user
+
+  def total_items
+    @todo_items ||= todo_items.count
+  end
+
+  def completed_items
+    @completed_items ||= todo_items.completed.count
+  end
+
+  def percent_complete  
+    return 0 if completed_items == 0
+
+    (100 * completed_items.to_f / total_items).round(1) 
+  end
 end
